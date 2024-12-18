ae449b222cfdc3ae943d08e6a4154f5cb30d266c
Nathan Sudiara <nathansudiara@gmail.com>
2024-12-18 22:20:39 +0700

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+// import peta from '../assets/Peta.svg'
+import peta2 from "../assets/PetaNigga.svg";
+import logo from "../assets/YukirNgab.svg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>ini home</h1>
+    <div className="bg-black h-screen w-screen flex p-16">
+      <div className="flex flex-col items-center justify-start w-full gap-8">
+        <img src={logo} alt="YukirNgab" />
+        <img src={peta2} alt="peta" />
+        <button className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+          Lihat Parkir 
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
