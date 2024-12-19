c12134db444955f50e90a57864f5da2678facf6b
Nathan Sudiara <nathansudiara@gmail.com>
2024-12-19 12:19:03 +0700

    refactor Home component: update styling and clean up unused imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-// import peta from '../assets/Peta.svg'
+import peta from "../assets/Peta.svg";
 import { useNavigate } from "react-router-dom";
-import peta2 from "../assets/PetaNigga.svg";
-import logo from "../assets/YukirNgab.svg";
+// import peta2 from "../assets/PetaNigga.svg";
+// import logo from "../assets/YukirNgab.svg";
 import useAuth from "../hooks/useAuth";
 
 const Home = () => {
@@ -16,11 +16,14 @@ const Home = () => {
   };
 
   return (
-    <div className="bg-gray-500 h-screen w-screen flex p-16">
+    <div className="bg-slate-200 h-screen w-screen flex p-16">
       <div className="flex flex-col items-center justify-start w-full gap-8">
-        <img src={logo} alt="YukirNgab" />
-        <img src={peta2} alt="peta" />
-        <button onClick={handleClick} className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+        <h1 className="text-4xl font-bold text-black">YukirNgab</h1>
+        <img src={peta} alt="peta" />
+        <button
+          onClick={handleClick}
+          className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+        >
           Lihat Parkir
         </button>
       </div>
